70a7afd213819c252d1d115a7c928b789b0d1c58
admin <admin@sifnos.lan>
2012-12-21 01:06:56 +0200

    Ok2

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Git !!");
+		System.out.println("Hello Git2 !!");
+		System.out.println("Hello Git3 !!");
 	}
 
 }
